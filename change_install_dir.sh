8b635c1dd6eebd92410d6c12fc12fa722da99e71
bsp2 <bs@tkscript.de>
2025-10-26 14:12:25 +0100

    add change_install_dir script

---
+++ change_install_dir.sh
@@ -0,0 +1,10 @@
+# change installation directory (m all_install)
+if [ -z "${1}" ]; then
+export TKS_PREFIX=/d/bsp/tools/tks/
+else
+export TKS_PREFIX="${1}"
+fi
+export TKS_SITE_PREFIX="${TKS_PREFIX}"
+export TKS_TARGET_PREFIX="${TKS_PREFIX}"
+export TKS_TARGET_SITE_PREFIX="${TKS_PREFIX}"
+echo "install dir set to \"${TKS_PREFIX}\""
