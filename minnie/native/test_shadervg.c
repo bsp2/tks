f07dfa9cf04d484bd0522e46ee210e20b9f6025c
bsp2 <bs@tkscript.de>
2026-03-10 08:36:45 +0100

    fix MSVC warning

--- minnie/native/test_shadervg.c
+++ minnie/native/test_shadervg.c
@@ -4554,7 +4554,7 @@ void hal_on_draw(void) {
    {
       glFinish();
       sU32 tDelta = hal_get_ticks() - ticks_start;
-      sF32 fps = (1000.0 * auto_exit_frames * numIter) / tDelta;
+      sF32 fps = (sF32)((1000.0 * auto_exit_frames * numIter) / tDelta);
       Dprintf("[...] auto_exit after %u frames (%u iteration(s)) / %u millisec => %3.2f fps\n", num_frames_rendered, auto_exit_num_iter, tDelta, ((sSI)(fps*100))/100.0f);
       hal_window_quit();
    }
