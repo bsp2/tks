09f5374a8e1151fd85b1ba15e02e5025998a10df
bsp2 <bs@tkscript.de>
2026-03-12 19:08:28 +0100

    use SDL2_net on macOS

--- tksdl_net/sdl_net_includes.h
+++ tksdl_net/sdl_net_includes.h
@@ -1,13 +1,13 @@
 /// sdl_net_includes.h
 ///
-/// (c) 2008-2009 Bastian Spiegel <bs@tkscript.de>
+/// (c) 2008-2026 Bastian Spiegel <bs@tkscript.de>
 ///     - Distributed under terms of the Lesser GNU General Public License (LGPL).
 ///       See COPYING and <http://www.gnu.org/licenses/licenses.html#LGPL> for further information.
 ///
 ///
 
-#ifndef __TKSDL_NET_INCLUDES_H__
-#define __TKSDL_NET_INCLUDES_H__
+#ifndef TKSDL_NET_INCLUDES_H__
+#define TKSDL_NET_INCLUDES_H__
 
 
 #ifdef YAC_WIN32
@@ -17,11 +17,11 @@
 
 #else
 
-#include <SDL/SDL.h>
-#include <SDL/SDL_net.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_net.h>
 
 #endif // YAC_WIN32
 
 
 
-#endif // __TKSDL_NET_INCLUDES_H__
+#endif // TKSDL_NET_INCLUDES_H__
