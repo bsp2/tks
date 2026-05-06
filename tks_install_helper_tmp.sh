da86685e88780d4a992e5bab67dd889b969da8fe
bsp2 <bs@tkscript.de>
2026-05-06 23:14:23 +0200

    residue

--- tks_install_helper_tmp.sh
+++ tks_install_helper_tmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TKS_WWW_PATH=http://tkscript.org/files/tmp/28Feb2026/
+TKS_WWW_PATH=http://tkscript.org/files/tmp/26Apr2026/
 ##TKS_WWW_PATH=http://tkscript.org/files/current/
 
 # (note) TKS_PACKAGES lists all files to be downloaded / unzipped
