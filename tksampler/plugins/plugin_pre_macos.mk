398c2a7ac5f7934883fe6b9aa65cdb509cd2a0cb
bsp2 <bs@tkscript.de>
2026-05-21 18:51:06 +0200

    pass STFX_SKIP_MAIN_INIT to sub makefiles

--- tksampler/plugins/plugin_pre_macos.mk
+++ tksampler/plugins/plugin_pre_macos.mk
@@ -5,6 +5,11 @@ ifeq ($(TARGET_DIR),)
 TARGET_DIR:=$(TKS_ROOT)/tks-projects/apps/eureka/voice_plugins/
 endif
 
+ifneq (${STFX_SKIP_MAIN_INIT},)
+CFLAGS   += -DSTFX_SKIP_MAIN_INIT
+CPPFLAGS += -DSTFX_SKIP_MAIN_INIT
+endif
+
 CFLAGS+= -Wno-unused-variable -Wno-unused-function
 CPPFLAGS+= -Wno-unused-variable -Wno-unused-function
 EXTRALIBS+=
--- tksampler/plugins/plugin_pre_msvc.mk
+++ tksampler/plugins/plugin_pre_msvc.mk
@@ -5,4 +5,9 @@ ifeq ($(TARGET_DIR),)
 TARGET_DIR:=$(TKS_ROOT)/tks-projects/apps/eureka/voice_plugins/
 endif
 
+ifneq (${STFX_SKIP_MAIN_INIT},)
+CFLAGS   += -DSTFX_SKIP_MAIN_INIT
+CPPFLAGS += -DSTFX_SKIP_MAIN_INIT
+endif
+
 EXTRALIBS += -DLL -MAP
