696789ec2507f38736cd06a5b879a3a6b37071cf
bsp2 <bs@tkscript.de>
2026-08-20 10:29:58 +0200

    add apiref example

---
+++ tks-projects/tools/apiref/test.h
@@ -0,0 +1,41 @@
+
+/* -----------------------------------------------------------------------------
+ * Synopsis text.
+ *
+ * Long explanation here.
+ *
+ * Can span multiple lines, may contain markdown text (e.g. `preformatted`).
+ *
+ *       note: a note
+ *  important: don't feed after midnight
+ *       todo: implement me
+ *
+ *  Parameters:
+ *          x : horizontal position
+ *          y : vertical position
+ *
+ *  Returns:
+ *   Return value description here.
+ *
+ * See also:
+ *   rect_area
+ *
+ * -----------------------------------------------------------------------------
+ */
+extern float myfun (float _x, float _y);
+
+
+/* -----------------------------------------------------------------------------
+ * Calculate rectangle area.
+ *
+ * Uses default argument documentation (see autodoc_test.ini).
+ *
+ *  Returns:
+ *   Returns retangle area
+ *
+ * See also:
+ *   myfun
+ *
+ * -----------------------------------------------------------------------------
+ */
+extern float rect_area (float _sx, float _sy);
---
+++ tks-projects/tools/apiref/test.md
@@ -0,0 +1,29 @@
+% apiref test document
+% bsp
+% 20-Aug-2026
+>>>
+20Aug2026
+<<<
+
+$(var:header)
+
+# Table of contents
+
+$(buf!toc)
+
+# test.h
+
+This is a test for the apiref "test" module.
+
+`apiref` scans the functions (and function pointer) definitions in a C header file and converts them to markdown documentation.
+
+See [test.h](test.h) for the example header source.
+
+$(inc:autogen__apiref.md)
+
+
+$(buf!footnote)
+$(buf!w3validator)
+
+Document created in $(var:gen_ms) on $(var:localdatetime)
+].create
